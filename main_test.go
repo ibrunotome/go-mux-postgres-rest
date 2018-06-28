cb715e67bf03fdd4bb547f1c87a8884921c99601
Bruno Tomé <ibrunotome@gmail.com>
2018-06-28 11:05:21 -0300

    Add first test

--- main_test.go
+++ main_test.go
@@ -7,6 +7,8 @@ import (
 	"."
 	"log"
 	"github.com/joho/godotenv"
+	"net/http"
+	"net/http/httptest"
 )
 
 var a main.App
@@ -31,6 +33,32 @@ func TestMain(m *testing.M) {
 	clearTable()
 
 	os.Exit(code)
+}
+
+func TestEmptyTable(t *testing.T) {
+	clearTable()
+
+	req, _ := http.NewRequest("GET", "/products", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	if body := response.Body.String(); body != "[]" {
+		t.Errorf("Expected an empty array. Got %s", body)
+	}
+}
+
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	a.Router.ServeHTTP(rr, req)
+
+	return rr
+}
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+	}
 }
 
 func ensureTableExists() {
